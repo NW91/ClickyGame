40822ad1e31922309ddbfbb884fa4c4f7c8665b7
Nicole W <nicoleewhitee@gmail.com>
2019-05-06 21:48:55 -0700

    Updates

---
+++ precache-manifest.72f0ff40c24a8e02b15c2b8ddd2ae677.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc31c6872391123baa3eee53c3b759c",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "c0e3b7b0d9be925ac648",
+    "url": "/ClickyGame/static/css/main.de0fda85.chunk.css"
+  },
+  {
+    "revision": "2b5b43459bd67b730b0e",
+    "url": "/ClickyGame/static/js/2.0ab779d2.chunk.js"
+  },
+  {
+    "revision": "c0e3b7b0d9be925ac648",
+    "url": "/ClickyGame/static/js/main.46642e7a.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
